9dbcce3fee87501ad2cbbdac13c00d8f78ec3a7c
shephbra000 <shephbra000@warren.k12.in.us>
2024-08-29 08:20:14 -0400

    initial commit

--- o.py
+++ o.py
@@ -1,4 +1,4 @@
 frisbees = 3
 COST_OF_FRISBEE = 15
-total_cost = 45
-(print("The total cost is $45") , total_cost,sep="45")
+total_cost = frisbees * COST_OF_FRISBEE
+print("The total cost is $", total_cost, sep="")
